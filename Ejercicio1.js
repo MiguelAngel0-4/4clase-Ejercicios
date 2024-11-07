d58b255d266f5201250f250f7081b97e8590c39f
Miguel <migue2801224@gmail.com>
2024-11-06 20:30:47 -0500

    Primer ejercicio

---
+++ Ejercicio1.js
@@ -0,0 +1,19 @@
+class Rectangulo{
+    constructor(ancho, alto){
+        this.ancho=8;
+        this.alto=4;
+    }
+
+    //metodo para calcular el area
+    area(){
+        return this.ancho * this.alto;
+    }
+
+    perimetro(){
+        return 2 * (this.ancho + this.alto);
+    }
+}
+
+let rectangulo= new Rectangulo();
+console.log("El Area del rectangulo es:",rectangulo.area());
+console.log("El Perimetro del rectangulo es:",rectangulo.perimetro());
